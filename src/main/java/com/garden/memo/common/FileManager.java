0301028c24dc9cee1da484d4b7a84526394b9618
garden <lmnoptnt@gmail.com>
2024-09-25 02:29:39 +0900

    [홍정원] - 메모 수정 API - 메모 수정 기능 #4
    - 메모 삭제 API
    - 메모 삭제 기능
    - 메모 파일, 디렉토리 삭제 기능
    #5

--- src/main/java/com/garden/memo/common/FileManager.java
+++ src/main/java/com/garden/memo/common/FileManager.java
@@ -60,4 +60,30 @@ public class FileManager {
 	}
 	
 	
+	public static boolean removeFile (String filePath) { // /images/2_1726054029039/cat-8275147_640.jpg
+
+		if(filePath == null) {
+			return false;
+		}
+		
+		String fullFilePath = FILE_UPLOAD_PATH + filePath.replace("/images", "");
+		
+		Path path = Paths.get(fullFilePath);
+		Path directioryPath = path.getParent();
+		
+		// 파일과 폴더(디렉토리) 존재하는지 확인
+		if(Files.exists(path) && Files.exists(directioryPath)) {
+			try {
+				Files.delete(path);
+				Files.delete(directioryPath);
+			} catch (IOException e) {
+				return false;
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
 }
--- src/main/java/com/garden/memo/post/service/PostService.java
+++ src/main/java/com/garden/memo/post/service/PostService.java
@@ -45,7 +45,42 @@ public class PostService {
 		
 		Post post = optionalPost.orElse(null);
 		
-		return post;
+		return post; 
 	}
+	
+	public Post updatePost(int id, String title, String contents) {
+		
+		Optional<Post> optionalPost = postRepository.findById(id);
+		
+		Post post = optionalPost.orElse(null);
+		
+		if(post != null) {
+			Post updatePost = post.toBuilder()
+							.title(title)
+							.contents(contents)
+							.build();
+			return postRepository.save(updatePost);
+		} else {
+			return null;
+		}
+		
+	}
+	
+	public boolean deletePost(int id) {
+		// postRepository.deleteById(id);
+		Optional<Post> optionalPost = postRepository.findById(id);
+		
+		Post post = optionalPost.orElse(null);
+		
+		if(post != null) {
+//		/images/2_1726054029039/cat-8275147_640.jpg
+			FileManager.removeFile(post.getImagePath());
+			postRepository.delete(post);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	
 }
