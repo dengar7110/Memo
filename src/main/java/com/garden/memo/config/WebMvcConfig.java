ef024567df7e5abdf10d1a9cddda91a962e08468
garden <lmnoptnt@gmail.com>
2024-09-29 00:42:19 +0900

    [홍정원] - 로그인 여부에 따라 관련 페이지 접근 제어

--- src/main/java/com/garden/memo/config/WebMvcConfig.java
+++ src/main/java/com/garden/memo/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.garden.memo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.garden.memo.common.FileManager;
+import com.garden.memo.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -14,5 +16,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
 	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		PermissionInterceptor interceptor = new PermissionInterceptor();
+	
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**")
+		.excludePathPatterns("/static/**", "/images/**", "/user/logout");
+	
+	}
+	
 	
 }
